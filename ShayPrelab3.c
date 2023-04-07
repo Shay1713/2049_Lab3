17c0a550629c5cb9f8f2109b426469a0bc2bb898
Shay1713 <116598440+Shay1713@users.noreply.github.com>
2023-04-06 22:19:10 -0400

    Update ShayPrelab3.c

--- ShayPrelab3.c
+++ ShayPrelab3.c
@@ -5,30 +5,31 @@
 
 
 // Declare globals here
-unsigned char currKey=0;
+unsigned char currKey;
 unsigned int length = 32;
-unsigned int currentLength = 4;
 unsigned int currentIndex = 0;
 int arrayIndex = 0;
+int count = 0;
+int i = 0;
 char note1;
 char note2;
-int count = 0;;
-int i = 0;
 //unsigned char dispSz = 0;
 
 // Functions
 void swDelay2(char numLoops);
-void sendSimon(int sequence, int length);
 int loadSequence(int sequence, int count);
-void printNum(char input);
 void printNum(char input);
 void countDown(void);
 void time2(int numLoops2, int c);
 int ledDecimal(int binaryInput);
-void S1toS4toLEDs(void);
+
+// Functions For Lab3
+void displayTime(long unsigned int inTime);
+void displayTemp(float inAvgTempC);
 
 
 // TIMER FUNCTIONS
+void S1toS4toLEDs(void);
 void swDelay(char numLoops);
 void startingA2Timer(void);
 void stoppingA2Timer(void);
@@ -36,18 +37,21 @@ void displayingA2Timer(unsigned long curr_time);
 void timerA2Function();
 void timerA2CountDown(void);
 void tellTheNote(char input);
+
 volatile unsigned long timer = 0;
+unsigned long start_time = 0;
+unsigned long current_time = 0;
+unsigned long elapsed_time = 0;
 char button_state;
-unsigned long elapsed_time;
 char timer_on;
-unsigned long start_time;
-unsigned long current_time;
 int sec;
 int min;
+int hour;
+int day;
+int month;
 int total_dec;
 int total_sec;
 
-unsigned long start_time = 0;
 #pragma vector=TIMER2_A0_VECTOR
 __interrupt void TimerA2_ISR(void)
 {
@@ -56,8 +60,6 @@ __interrupt void TimerA2_ISR(void)
         P1OUT ^= BIT0;
     }
 }
-
-char sequence[36] = {'d', 'b', 'b', 'c', 'b', 'b', 'a', 'a', 'd', 'd', 'a', 'a', 'a', 'b', 'a', 'a', 'g', 'g', 'd', 'b', 'b', 'b', 'c', 'b', 'a', 'a', 'd', 'd', 'a', 'a', 'a', 'b', 'a', 'a', 'g', 'g'};
 
 
 enum GAME_STATE {WELCOME, STARTING, SEQUENCE, SONGCHOICE, GAMEOVER, RESTART, WIN, TEST};
@@ -82,7 +84,7 @@ void main(void)
 
 
 
-    // Configure LED P1.0
+    // Configure LED P1.0 Red Flashing
     P1SEL &= ~BIT0;
     P1DIR |= BIT0;
     startingA2Timer();
@@ -96,7 +98,6 @@ void main(void)
         unsigned long current_time = timer;
         elapsed_time = current_time - start_time;
 
-        // Check if any keys have been pressed on the 3x4 keypad
         currKey = getKey();
 
         if (currKey == '#') { // to restart game at any time
@@ -109,161 +110,23 @@ void main(void)
                 S1toS4toLEDs();
             break;
 
-            case WELCOME:
-                BuzzerOff();
-                setLeds(0);
-                currKey = getKey();
-                Graphics_drawStringCentered(&g_sContext, "HERO", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT); // instructing player
-                Graphics_drawStringCentered(&g_sContext, "Press '*'", AUTO_STRING_LENGTH, 48, 25, TRANSPARENT_TEXT); // instructing player
-                Graphics_flushBuffer(&g_sContext);
-                if (currKey == '*') {
-                    state = SONGCHOICE;
-                }
+            case DISPLAYING:
+               
             break;
 
             case SONGCHOICE:
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                BuzzerOn();
-                Graphics_drawStringCentered(&g_sContext, "Song Choices", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "Today Are ...", AUTO_STRING_LENGTH, 48, 45, TRANSPARENT_TEXT);
-                Graphics_flushBuffer(&g_sContext);
-                swDelay2(5);
-                BuzzerOff();
-                Graphics_clearDisplay(&g_sContext); // Clear the display
-                Graphics_flushBuffer(&g_sContext);
-                BuzzerOn();
-                Graphics_drawStringCentered(&g_sContext, "Hush Little", AUTO_STRING_LENGTH, 48, 35, TRANSPARENT_TEXT); // instructing player
-                Graphics_drawStringCentered(&g_sContext, "Baby", AUTO_STRING_LENGTH, 48, 45, TRANSPARENT_TEXT); // instructing player
-                Graphics_flushBuffer(&g_sContext);
-                swDelay2(4);
-                BuzzerOff();
-                Graphics_clearDisplay(&g_sContext); // Clear the display
-                Graphics_flushBuffer(&g_sContext);
-                start_time = timer;
-                state = STARTING;
+               
             break;
 
-            case STARTING:
-                printf("Count Down Begins\n");
-                countDown();
-                Graphics_clearDisplay(&g_sContext); // Clear the display
-                Graphics_flushBuffer(&g_sContext);
-                arrayIndex = 36; // resetting for the sequence state
-                count = 0;
-                state = SEQUENCE;
+            case STARTING: 
 
             break;
 
             case SEQUENCE:
-                printf("Made It To Sequence\n");
-                for (i = 0; i < arrayIndex; i++) {
-                    Graphics_clearDisplay(&g_sContext); // Clear the display
-                    Graphics_flushBuffer(&g_sContext);
-                    setLeds(0);
-                    timer = 0;
-                    timer_on = 0;
-                    while(1){
-                        timer_on = 1;
-                        if (timer_on) {
-                            if ((timer % 10) == 0) {
-                                displayingA2Timer(timer);
-                            }
-                            if ((sec == 1) && (total_dec == 10)){ // playing the note
-                                tellTheNote(i);
-                                S1toS4toLEDs();
-                                printf("note1 is %c and note2 is %c\n", note1, note2);
-                                if ((note1 == '1' | '2' | '3' | '4') && (note2 == '1' | '2' | '3' | '4')){
-                                    break;
-                                }
-                            }
-                        }
-                    } // end while loop 1
-                    timer = 0;
-                    timer_on = 0;
-                    while(1){
-                        timer_on = 1;
-                        if (timer_on) {
-                            if ((timer % 10) == 0) {
-                                displayingA2Timer(timer);
-                            }
-                            if (((sec == 0) && (total_dec == 0)) || ((sec == 0) && (total_dec == 10))){ // playing the note
-                                if (note1 != note2){
-                                     count = 12;
-                                     printf("BAD ROUND\n");
-                                     break;
-                                 } else if (note1 == note2){
-                                     count = 0;
-                                     printf("good round\n");
-                                     break;
-                                 }
-                            }
-                        }
-                    } // end of while loop2
-                    if (count == 0){
-                        printf("good round\n");
-                    } else if (count == 12){
-                        printf("GAMEOVER");
-                        break;
-                    }
-
-                } // end of for loop
-
-                if (count == 0){
-                    printf("YOU WON\n");
-                    state = WIN;
-                } else if (count == 12){
-                    printf("YOU LOSE");
-                    state = GAMEOVER;
-                } // the user pressed all buttons correctly
-
-
+                
             break;
 
-            case GAMEOVER:
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                Graphics_drawStringCentered(&g_sContext, "GAME OVER", AUTO_STRING_LENGTH, 50, 35, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "YOU LOSE", AUTO_STRING_LENGTH, 50, 45, TRANSPARENT_TEXT);
-                Graphics_flushBuffer(&g_sContext); // refresh
-                swDelay2(5);
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                state = WELCOME;
-            break;
-
-            case RESTART:
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                Graphics_drawStringCentered(&g_sContext, "RESTART", AUTO_STRING_LENGTH, 50, 25, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "PROCESSING", AUTO_STRING_LENGTH, 50, 35, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "...", AUTO_STRING_LENGTH, 50, 45, TRANSPARENT_TEXT);
-                Graphics_flushBuffer(&g_sContext); // refresh
-                setLeds(0);
-                swDelay2(4);
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                state = WELCOME;
-           break;
-
-           case WIN:
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                Graphics_drawStringCentered(&g_sContext, "CONGRATS", AUTO_STRING_LENGTH, 50, 25, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "YOU WON", AUTO_STRING_LENGTH, 50, 35, TRANSPARENT_TEXT);
-                Graphics_drawStringCentered(&g_sContext, "HERO", AUTO_STRING_LENGTH, 50, 45, TRANSPARENT_TEXT);
-                Graphics_flushBuffer(&g_sContext); // refresh
-                for (count = 9000; count > 0; count--){
-                    setLeds('8' - 0x30);
-                    setLeds('4' - 0x30);
-                    setLeds('2' - 0x30);
-                    setLeds('1' - 0x30);
-                }
-                setLeds(0);
-                Graphics_clearDisplay(&g_sContext);
-                Graphics_flushBuffer(&g_sContext);
-                state = WELCOME;
-           break;
+           
         }
 
 
@@ -272,13 +135,6 @@ void main(void)
 
 
 // FUNCTIONS START HERE
-
-
-void countDown() {
-    timerA2CountDown();
-    BuzzerOff();
-    setLeds(0);
-}
 
 void printNum(char input) {
     unsigned char dispThree[3];
@@ -353,8 +209,8 @@ void displayingA2Timer(unsigned long curr_time){
     sec = total_sec % 60; // Seconds
     //unsigned char str[9]; // mm:ss.hh = 8 chars + null terminator
     //printf(str, 9, "%02d:%02d.%02d", min, sec, total_dec);
-    printf("minutes %d seconds %d total_dec %d\n", min, sec, total_dec);
-    //Graphics_drawStringCentered(&g_sContext, str, 9, 48, 15, OPAQUE_TEXT);
+    //printf("minutes %d seconds %d total_dec %d\n", min, sec, total_dec);
+    Graphics_drawStringCentered(&g_sContext, str, 9, 48, 15, OPAQUE_TEXT);
 }
 
 void timerA2Function(){
@@ -574,3 +430,36 @@ void S1toS4toLEDs(void) // Me setting the buttons to light the LEDs
             }
     }
 }
+
+void displayTime(long unsigned int inTime){ // for prelab3
+     // takes in global time parameter as input (timer)
+     // convert the seconds in the global timer count into 
+     // ... Date: MMM DD (ex. OCT 12)
+     // ... Time: HH:MM:SS (ex: 01:12:38)
+     // Display the date and time on the LCD Screen Top
+
+     total_sec =  curr_time / 100; // Seconds (integer part)
+     total_dec = curr_time % 100; // Hundredths (fractional part)
+     month = total_sec / 2592000; // Mongh (~ this many seconds 30 day average) 
+     day = total_sec / 86400; // Days
+     hour = total_sec / 3600; // Hours
+     min = total_sec / 60; // Minutes
+     sec = total_sec % 60; // Seconds
+     
+     //Graphics_drawStringCentered(&g_sContext, str, 9, 48, 15, OPAQUE_TEXT);
+     //unsigned char str[9]; // mm:ss.hh = 8 chars + null terminator
+     //printf(str, 9, "%02d:%02d.%02d", min, sec, total_dec);
+     //printf("minutes %d seconds %d total_dec %d\n", min, sec, total_dec);
+   
+
+}
+
+void displayTemp(float inAvgTempC) {
+     // takes in global temp parameter in °C and converts 
+     // ... Temp (C): ddd.f °C (ex. 124.5 °C)
+     // ... Temp (F): ddd.f °F (ex. 365.7 °F)
+     // Display the temp in C and F on the LCD Screen Bottom
+}
+
+
+
